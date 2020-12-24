caf798adc66775ff380ce73dbb91dd6fefebd2e5
Vitaliy622 <vitalikyarick@gmail.com>
2020-12-24 15:33:04 +0200

    Add new project

---
+++ src/main/java/com/neardySoftTest/neardy/NeardySoftTestApplication.java
@@ -0,0 +1,11 @@
+package com.neardySoftTest.neardy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeardySoftTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NeardySoftTestApplication.class, args);
+    }
+}
